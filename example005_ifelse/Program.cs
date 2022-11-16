26e200815f40ee83d162944923d89e919a62ff44
Max <maximizer38@gmail.com>
2022-11-16 19:31:03 +0700

    example 005 if else

---
+++ example005_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("вводи имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "максим")
+{
+    Console.WriteLine("ура, это же Максим");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
